23090a15e5f9be33b50f4e68f72a0c9148bee31c
bachirNdiaye19 <mouhamadoulbachirndiaye@esp.sn>
2023-02-21 23:22:58 +0000

    Ceci est un premier commit pour tester intelij

--- src/main/java/esp/bachir/model/Utilisateur.java
+++ src/main/java/esp/bachir/model/Utilisateur.java
@@ -6,7 +6,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="users")
+@Table(name="employe")
 public class Utilisateur {
 	
 	@Id
@@ -14,43 +14,7 @@ public class Utilisateur {
 	private int id;
 	private String nom;
 	private String prenom;
-	private String email;
-	private String password1;
-	private String adresse;
 	
-	public Utilisateur(int id, String nom, String prenom, String email, String password1, String adresse) {
-		super();
-		this.id = id;
-		this.nom = nom;
-		this.prenom = prenom;
-		this.email = email;
-		this.password1 = password1;
-		this.adresse = adresse;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword1() {
-		return password1;
-	}
-
-	public void setPassword1(String password1) {
-		this.password1 = password1;
-	}
-
-	public String getAdresse() {
-		return adresse;
-	}
-
-	public void setAdresse(String adresse) {
-		this.adresse = adresse;
-	}
 
 	public Utilisateur(int id, String nom, String prenom) {
 		super();
